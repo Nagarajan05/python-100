537b137b2ba987a1f67a348f2a80cdd80dff63b7
Nagarajan Ramakrishnan <senthilkumarnagarajan1@gmail.com>
2021-05-21 03:17:46 +0530

    Day 1 - append
    Lol

--- BandNameGenerator.py
+++ BandNameGenerator.py
@@ -1,2 +1,14 @@
-statement = "Hello 3rd Rock from the space"
-print(statement)
+#1. Create a greeting for your program.
+print("Hi, Lead Band - Lets give you an awesome name")
+
+#2. Ask the user for the city that they grew up in.
+user_city_name = input("Which city did you grew up in?\n")
+
+#3. Ask the user for the name of a pet.
+user_pet_name = input("What is the name of your pet?\n")
+
+#4. Combine the name of their city and pet and show them their band name.
+print("Your awesome Band name is "+user_city_name + " " + user_pet_name)
+
+#5. Make sure the input cursor shows on a new line, see the example at:
+#   https://band-name-generator-end.appbrewery.repl.run/
